97771547a64be274132b9ad2e4b6e5a8f2f17061
Winona <winona.p.pangestu@gmail.com>
2025-05-02 23:30:55 +0700

    Updated footer's responsiveness

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,20 +3,20 @@ import Image from 'next/image'
 
 const Footer = () => {
     return (
-        <div className='bg-[#EAB139] mt-40 flex flex-row w-full'>
+        <div className='bg-[#EAB139] mt-40 flex flex-col md:flex-row w-full'>
             {/* Left side */}
-            <div className='bg-[#030C19] py-14 px-8 w-1/3 flex justify-start items-center'>
+            <div className='bg-[#030C19] py-14 px-8 w-full md:w-1/3 flex justify-start items-center'>
                 <Image
                     src="/images/footer-logo-and-quote.png"
                     alt=""
                     width={2100}
                     height={0}
-                    className="w-[400px]"
+                    className="w-[300px] md:w-[400px]"
                 />
             </div>
 
             {/* Right side (social link lists) */}
-            <div className='py-14 px-8 w-2/3 text-[#030C19]'>
+            <div className='py-14 px-8 w-full md:w-2/3 text-[#030C19]'>
                 <span className='passion-one-bold text-2xl select-none'>CONNECT WITH US!</span>
                 <ul className='flex flex-col gap-1'>
                     <li><span className='cursor-pointer hover:underline'>Instagram</span></li>
