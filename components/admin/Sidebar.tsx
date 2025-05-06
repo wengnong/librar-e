8880a77c5e3edb9bcb12e8a9ec13c81841d377b8
notbenjackson <arielraynara@gmail.com>
2025-05-06 10:14:12 +0700

    update sidebar (ray)
    progress 1

--- components/admin/Sidebar.tsx
+++ components/admin/Sidebar.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Sidebar = () => {
     return (
-        <div>Sidebar</div>
+        <div>
+
+            <hr className="my-5 h-1 border-t-0 bg-amber-200" />
+            <div className='flex-col p-6'>
+                <h1 className='text-center text-shadow-white text-2xl'>Username</h1>
+                <p className='text-center'>emailuser@gmail.com</p>
+            </div>
+            <hr className="my-5 h-1 border-t-0 bg-amber-200" />
+
+            <div className=''>
+                <div>
+                    
+                </div>
+
+            </div>
+        </div>
+            
     )
 }
 
