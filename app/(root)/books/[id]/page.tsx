9cd4192645f0bfde68174eb127eb717a6fe73a20
Winona <winona.p.pangestu@gmail.com>
2025-05-08 23:39:16 +0700

    Create page.tsx for books section
    Starting point; rough idea for working later on

---
+++ app/(root)/books/[id]/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>
+            {/* bakal contain details bukunya, dan kalo bukunya di klik dari home page ato library,
+                bakal di redirect ke sini. data buku bakal diambil dari database.
+                
+                note: perlu drizzle, schema, redirect pake next/navigation,
+                lalu bikin component khusus untuk buku buat masukkin semua requirementsnya.
+            */}
+        </div>
+    )
+}
+
+export default page
