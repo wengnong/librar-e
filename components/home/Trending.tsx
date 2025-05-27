50713debe6071146c59a4156405ed4f058ce261d
Winona <winona.p.pangestu@gmail.com>
2025-05-27 19:10:00 +0700

    update: book filtering for trending and most readers' choice based on rating and latest published date

--- components/home/Trending.tsx
+++ components/home/Trending.tsx
@@ -17,6 +17,7 @@ const Trending = ({
     return (
         <div className='relative flex flex-col justify-center items-center w-full my-40 px-10'>
             <h1 className='z-20 paytone-one-regular uppercase text-3xl text-[#EAB139] shadow-md'>{title}</h1>
+            <span className='mt-2 max-w-4xl text-center text-gray-300 leading-relaxed px-4 md:px-0'>Discover the freshest arrivals and hottest books making waves right now!</span>
 
             {/* List of trending books */}
             <ul className='flex flex-wrap justify-center gap-8 mt-8'>
